b443856e48f97a32b228c180b6f3f088c8d99547
Justin Knight <knightjknight@gmail.com>
2019-01-22 13:57:21 -0600

    Initial Commit

---
+++ imagePickerTests/imagePickerTests.swift
@@ -0,0 +1,34 @@
+//
+//  imagePickerTests.swift
+//  imagePickerTests
+//
+//  Created by Justin Knight on 1/22/19.
+//  Copyright © 2019 JustinKnight. All rights reserved.
+//
+
+import XCTest
+@testable import imagePicker
+
+class imagePickerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
